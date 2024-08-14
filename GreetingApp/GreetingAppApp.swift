9d404c18da70ba1e3573df11746881a8c3db8284
Max <88677470+Max2450@users.noreply.github.com>
2024-08-14 00:34:12 -0700

    Getting Started

---
+++ GreetingApp/GreetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingAppApp.swift
+//  GreetingApp
+//
+//  Created by Max G on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
